2b3a0a710b6812a07e2bc48718149f62c6f507be
Tim <Tim@Tim-PC>
2013-12-16 10:19:40 -0600

    initial commit

---
+++ Senior_Hamlet/src/HamletMain.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class HamletMain 
+{
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Welcome to Hamlet", "Hamlet Game", -1);
+		JOptionPane.showMessageDialog(null, "This is the project", "Hamlet Game", -1);
+	}
+}
