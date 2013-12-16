3b39323fcf9ff98a0819010beeae680b845ba431
Tim <Tim@Tim-PC>
2013-12-16 10:39:05 -0600

    other initial commit

--- Senior_Hamlet/src/HamletMain.java
+++ Senior_Hamlet/src/HamletMain.java
@@ -1,10 +1,25 @@
 import javax.swing.JOptionPane;
 
-public class HamletMain 
-{
-	public static void main(String[] args)
-	{
-		JOptionPane.showMessageDialog(null, "Welcome to Hamlet", "Hamlet Game", -1);
-		JOptionPane.showMessageDialog(null, "This is the project", "Hamlet Game", -1);
+public class HamletMain {
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "Welcome to Hamlet", "Hamlet Game",
+				-1);
+
+		Object[] characters = { "Hamlet", "Horatio", "Claudius" };
+		int firstChoice = JOptionPane.showOptionDialog(null,
+				"Who would you like to play as?", "Character Option Screen",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+				null, characters, characters[2]);
+		
+		if(firstChoice == 0)
+		{
+			
+		}
+		else if(firstChoice == 1)
+		{
+			
+		}
+		else if(firstChoice == 2)
 	}
 }
